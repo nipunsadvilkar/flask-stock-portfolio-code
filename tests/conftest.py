23582b0d789a7b985c3c1950274231a1d0ec7870
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2022-04-02 12:39:15 +0530

    Pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import flask
+import pytest
+from flask import current_app
+
+from project import create_app
+
+
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app()
+    flask_app.config.from_object('config.TestingConfig')
+
+    with flask_app.test_client() as testing_client:
+
+        with flask_app.app_context():
+            current_app.logger.info("Inside a testing fixture...")
+
+        yield testing_client
--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -3,27 +3,26 @@ This file (test_app.py) contains unit tests for the app.py file
 """
 from app import app
 
-def test_index_page():
+def test_index_page(test_client):
     """
     GIVEN a Flask Application
     WHEN the `/` is requested (GET)
     THEN check the response is valid
     """
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code == 200
-        assert b'Flask Stock Portfolio App' in response.data
-        assert b'Welcome to the Flask Stock Portfolio App!' in response.data
+    response = test_client.get('/')
+    assert response.status_code == 200
+    assert b'Flask Stock Portfolio App' in response.data
+    assert b'Welcome to the' in response.data
+    assert b'Flask Stock Portfolio App!' in response.data
 
-def test_about():
+def test_about(test_client):
     """
     GIVEN a Flask Application
     WHEN the `/` is requested (GET)
     THEN check the response is valid
     """
-    with app.test_client() as client:
-        response = client.get('/about')
-        assert b'Flask Stock Portfolio App' in response.data
-        assert b'About' in response.data
-        assert b'This application is built using Flask web framework' in response.data
-        assert b'Course is developed by Testdriven.io.' in response.data
+    response = test_client.get('/users/about')
+    assert b'Flask Stock Portfolio App' in response.data
+    assert b'About' in response.data
+    assert b'This application is built using Flask web framework' in response.data
+    assert b'Course is developed by Testdriven.io.' in response.data
